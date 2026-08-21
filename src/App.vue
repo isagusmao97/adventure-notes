0a8daca068193596cf8eb413c8c87560ba52fea8
isagusmao97 <isabelagusmao93@gmail.com>
2026-08-20 22:20:59 -0300

    Inicio do projeto de bloco de notas personalizado em livro de rpg.

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+ <div class="p-12">
+  <p class="text-md text-pink-400 text-center font-semibold font-mono">Hello Isabella! This will be a note pad named as Adventure Notes, you will discover why this name... <br/> See you soon! ⚔️ 🎒 🔮 ✨</p>
+ </div>
+</template>
